548c699fa01c3a91702aae330c422739ce1ffc94
alex belle <belletskiy75@mail.com>
2024-02-26 00:10:26 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишете програму, которая принимает на вход число
+//и проверяет кратно оно одновременно 7 и 23.
+
+int num = int.Parse(Console.ReadLine()!);
+
+if (num % 7 == 0 && num % 23 == 0)
+
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
