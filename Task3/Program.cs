d81edaeddb7bc3acb754c1f471a692d1738587fb
alex belle <belletskiy75@mail.com>
2024-02-26 17:36:02 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход целое число
+// из отрезка [10,99] и показывает максимальную цифру числа.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine()!);
+
+        if (N > 10 && N < 99)
+        {
+            int a = N / 10;
+            int b = N % 10;
+            if (a > b)
+
+                Console.Write(a);
+
+            else
+
+                Console.Write(b);
+        }
+    }
+}
