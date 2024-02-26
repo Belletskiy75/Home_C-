f6fabff996ea8fb03f5500ce44d69e74a48223e6
alex belle <belletskiy75@mail.com>
2024-02-26 16:44:22 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты тоски(X,Y),
+//прочем X#0 Y#0, и выдает номер координатной четверти, в которой находится точка.
+
+int X = int.Parse(Console.ReadLine()!);
+int Y = int.Parse(Console.ReadLine()!);
+
+if (X > 0 && Y > 0)
+
+    Console.WriteLine("1");
+
+else if (X < 0 && Y > 0)
+
+            Console.WriteLine("2");
+
+    else if (X < 0 && Y < 0)
+
+           Console.WriteLine("3");
+
+     else if (X > 0 && Y < 0)
+
+           Console.WriteLine("4");
